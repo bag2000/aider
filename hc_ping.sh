181ad3b9f1daa292904ed6881da4100c32f2d20c
Roman <polyakov.r.v@yandex.ru>
2026-01-21 13:57:56 +0300

    modified:   hc_ping.sh

--- hc_ping.sh
+++ hc_ping.sh
@@ -11,17 +11,20 @@ source "${SCRIPT_DIR}/log.sh" || { echo "Failed to load log.sh"; exit 1; }
 ID_FILE_PATH="${SCRIPT_DIR}/${HC_PING_ID_FILE}"
 
 # Проверка существования файла с UUID
-if [ -f "$ID_FILE_PATH" ]; then
-    log_info "[$HC_PING_LOGNAME] Поучаю UUID из $ID_FILE_PATH"
-    uuid=$(cat $ID_FILE_PATH)   # Чтение UUID из файла
-    log_info "[$HC_PING_LOGNAME] Получен UUID: $uuid"
-else
-    log_error "[$HC_PING_LOGNAME] Файл $ID_FILE_PATH не существует"
-    exit 1 # Выход если файл не найден
-fi
+_chech_id_file() {
+    if [ -f "$ID_FILE_PATH" ]; then
+        log_info "[$HC_PING_LOGNAME] Поучаю UUID из $ID_FILE_PATH"
+        uuid=$(cat $ID_FILE_PATH)   # Чтение UUID из файла
+        log_info "[$HC_PING_LOGNAME] Получен UUID: $uuid"
+    else
+        log_error "[$HC_PING_LOGNAME] Файл $ID_FILE_PATH не существует"
+        exit 1 # Выход если файл не найден
+    fi
+}
 
 # Функция отправки start ping (начало выполнения задачи)
 hc_ping_start() {
+    _chech_id_file
     local message="${1:-}"
     log_info "[$HC_PING_LOGNAME] Отправляю start ping в $HC_PING_URL/$uuid"
     response=$(curl --resolve $HC_PING_DNS --max-time 10 -fs -X POST --data-raw "${message}" "$HC_PING_URL/$uuid/start")
@@ -36,6 +39,7 @@ hc_ping_start() {
 
 # Функция отправки success ping (успешное завершение задачи)
 hc_ping_success() {
+    _chech_id_file
     local message="${1:-}"
     log_info "[$HC_PING_LOGNAME] Отправляю succes ping в $HC_PING_URL/$uuid"
     response=$(curl --resolve $HC_PING_DNS --max-time 10 -fs -X POST --data-raw "${message}" "$HC_PING_URL/$uuid")
@@ -50,6 +54,7 @@ hc_ping_success() {
 
 # Функция отправки fail ping (ошибка выполнения задачи)
 hc_ping_fail() {
+    _chech_id_file
     local message="${1:-}"
     log_info "[$HC_PING_LOGNAME] Отправляю fail ping в $HC_PING_URL/$uuid"
     response=$(curl --resolve $HC_PING_DNS --max-time 10 -fs -X POST --data-raw "${message}" "$HC_PING_URL/$uuid/fail")
