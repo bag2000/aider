1c241e90ead74bbcb3f3c7ab23fa078022382385
Roman <polyakov.r.v@yandex.ru>
2026-01-21 13:54:25 +0300

    modified:   main.sh

--- main.sh
+++ main.sh
@@ -5,6 +5,8 @@ source "${SCRIPT_DIR}/log.sh" || { echo "Failed to load log.sh"; exit 1; }
 source "${SCRIPT_DIR}/hc_create.sh" || { echo "Failed to load hc_create.sh"; exit 1; }
 source "${SCRIPT_DIR}/hc_ping.sh" || { echo "Failed to load hc_ping.sh"; exit 1; }
 
+# Переменная для лога ошибок
+# В конце она отправляется в чек
 main_check=""
 
 # Создаю чек
@@ -14,11 +16,8 @@ hc_create
 hc_ping_start "Начало резервного копирования"
 
 # Создать дамп progress
-# export BACKUP_POSTGRES_LOGNAME="BACKUP_POSTGRESS"
-# export BACKUP_POSTGRES_USER="postgres"
-# export BACKUP_POSTGRES_DIR="/bak/db"
 # $SCRIPT_DIR/backup_postgres.sh "alldb" || main_check+="ОШИБКА postgres alldb "
-# $SCRIPT_DIR/backup_postgres.sh "navigator" || main_check+="ОШИБКА postgres navigator "
+# $SCRIPT_DIR/backup_postgres.sh "namedb" || main_check+="ОШИБКА postgres navigator "
 
 if [ -n $main_check ]; then
     hc_ping_success "Резервное копирование успешно завершено"
