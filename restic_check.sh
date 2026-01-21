d5e3f6fa4fdf97382c6f17d1f7b85f5dba9d9973
Roman <polyakov.r.v@yandex.ru>
2026-01-21 15:56:02 +0300

    modified:   main.sh

--- restic_check.sh
+++ restic_check.sh
@@ -3,32 +3,34 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/log.sh" || { echo "Failed to load log.sh"; exit 1; }
 
-log_info "[$RESTIC_CHECK_LOGNAME] Создание директории для кэша: ${RESTIC_CACHE_DIR}"
-sudo mkdir -p "${RESTIC_CACHE_DIR}" || {
-    log_error "[$RESTIC_CHECK_LOGNAME] Не удалось создать директорию ${RESTIC_CACHE_DIR}"
-    exit 1
-}
-
-day=$(date +%d)
-
-if [ "$day" -le 28 ]; then
-    log_info "Начинаю проверку репозитория $RESTIC_REPO"
-    export RESTIC_PASSWORD
-    systemd-run --scope \
-    -p CPUQuota=100% \
-    -p MemoryLimit=1G \
-    -- restic \
-    check \
-    --read-data-subset $day/28 \
-    -o rest.connections=20 \
-    -r $RESTIC_REPO \
-    --cache-dir $RESTIC_CACHE_DIR \
-    &>> $LOG_PATH || {
-        log_error "Ошибка при проверке репозитория $RESTIC_REPO"
+restic_check() {
+    log_info "[$RESTIC_CHECK_LOGNAME] Создание директории для кэша: ${RESTIC_CACHE_DIR}"
+    sudo mkdir -p "${RESTIC_CACHE_DIR}" || {
+        log_error "[$RESTIC_CHECK_LOGNAME] Не удалось создать директорию ${RESTIC_CACHE_DIR}"
         exit 1
     }
-else
-    log_info "Текущее число $day больше 28. Пропускаю проверку. Слудующая проверка 1 числа."
-fi
 
-log_info "Проверка репозитория $RESTIC_REPO завершена"
+    day=$(date +%d)
+
+    if [ "$day" -le 28 ]; then
+        log_info "Начинаю проверку репозитория $RESTIC_REPO"
+        export RESTIC_PASSWORD
+        systemd-run --scope \
+        -p CPUQuota=100% \
+        -p MemoryLimit=1G \
+        -- restic \
+        check \
+        --read-data-subset $day/28 \
+        -o rest.connections=20 \
+        -r $RESTIC_REPO \
+        --cache-dir $RESTIC_CACHE_DIR \
+        &>> $LOG_PATH || {
+            log_error "Ошибка при проверке репозитория $RESTIC_REPO"
+            exit 1
+        }
+    else
+        log_info "Текущее число $day больше 28. Пропускаю проверку. Слудующая проверка 1 числа."
+    fi
+
+    log_info "Проверка репозитория $RESTIC_REPO завершена"
+}
