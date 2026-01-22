1bf3bce6d4035ac7fb524ce41c6495f8ddf13044
Roman <polyakov.r.v@yandex.ru>
2026-01-22 17:40:46 +0300

    modified:   main.sh

--- main.sh
+++ main.sh
@@ -6,6 +6,7 @@ source "${SCRIPT_DIR}/hc_create.sh" || { echo "Failed to load hc_create.sh"; exi
 source "${SCRIPT_DIR}/hc_ping.sh" || { echo "Failed to load hc_ping.sh"; exit 1; }
 source "${SCRIPT_DIR}/restic_check.sh" || { echo "Failed to load restic_check.sh"; exit 1; }
 source "${SCRIPT_DIR}/restic_backup.sh" || { echo "Failed to load restic_backup.sh"; exit 1; }
+source "${SCRIPT_DIR}/restic_forget.sh" || { echo "Failed to load restic_forget.sh"; exit 1; }
 
 log_info "*************************************************************************************"
 # Переменная для лога ошибок
@@ -24,6 +25,7 @@ hc_ping_start "Начало резервного копирования"
 
 # restic_check || main_check+="ОШИБКА restic check "
 # restic_backup || main_check+="ОШИБКА restic backup "
+restic_forget || main_check+="ОШИБКА restic forget "
 
 if [ -z "$main_check" ]; then
     hc_ping_success "Резервное копирование успешно завершено"
